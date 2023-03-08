573de454d971329844e7c4b1c6af9e14efc1b7a7
Soumajyoti <soumyabwn3@gmail.com>
2023-03-09 00:22:22 +0530

    adding fetch into newsMonkey

--- src/components/News.js
+++ src/components/News.js
@@ -40,15 +40,22 @@ export class News extends Component {
         }
     }
 
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=7531fcc04fc2428e92725eac12bae5f7";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({ articles: parsedData.articles })
+    }
+
     render() {
         return (
             <div className="container my-5">
-                <h2>NewsMonkey - Top Headings</h2>
+                <h1>NewsMonkey - Top Headings</h1>
 
                 <div className="row my-3" >
                     {this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title.slice(0, 40)} description={element.description.slice(0, 85)} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            <NewsItem title={element.title ? element.title.slice(0, 40) : ""} description={element.description ? element.description.slice(0, 85) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
                     })}
                 </div>
