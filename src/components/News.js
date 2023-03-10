47fa8a8b6b819b3d87d4974305201a868d9c4b23
Soumajyoti <soumyabwn3@gmail.com>
2023-03-10 20:30:32 +0530

    adding more features

--- src/components/News.js
+++ src/components/News.js
@@ -16,17 +16,23 @@ export class News extends Component {
         category: PropTypes.string,
     }
 
-    constructor() {
-        super();
+    capitalize = (str) => {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    constructor(props) {
+        super(props);
         this.state = {
             page: 1,
             articles: [],
             loading: false
         }
+
+        document.title = `${this.capitalize(this.props.category)} - NewsMonkey`;
     }
 
-    async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7531fcc04fc2428e92725eac12bae5f7&page=1&pageSize=${this.props.pageSize}`;
+    async updateNews() {
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7531fcc04fc2428e92725eac12bae5f7&page=${this.state.page}&pageSize=${this.props.pageSize}`;
 
         this.setState({ loading: true })
 
@@ -39,55 +45,39 @@ export class News extends Component {
         })
     }
 
+    async componentDidMount() {
+        this.updateNews();
+    }
+
     handlePreviousClick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7531fcc04fc2428e92725eac12bae5f7&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-
-        this.setState({ loading: true })
-
-        let data = await fetch(url);
-        let parsedData = await data.json();
-
         this.setState({
-            loading: false,
-            page: this.state.page - 1,
-            articles: parsedData.articles
+            page: this.state.page - 1
         })
+        this.updateNews();
     }
 
     handleNextClick = async () => {
-        if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=7531fcc04fc2428e92725eac12bae5f7&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-
-            this.setState({ loading: true })
-
-            let data = await fetch(url)
-            let parsedData = await data.json();
-
-            this.setState({
-                loading: false,
-                page: this.state.page + 1,
-                articles: parsedData.articles
-            })
-            { console.log("In handle NExt Click function: ", Math.ceil(this.state.totalResults / this.props.pageSize)) }
-        }
+        this.setState({
+            page: this.state.page + 1
+        })
+        this.updateNews()
     }
 
     render() {
         return (
             <div className="container my-5">
-                <h1 className='text-center'>NewsMonkey - Top Headings</h1>
+                <h1 className='text-center'>NewsMonkey - Top {this.capitalize(this.props.category)} Headings</h1>
                 {this.state.loading && <Spinner />}
                 <div className="row my-3" >
                     {!this.state.loading && this.state.articles && this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title ? element.title.slice(0, 40) : ""} description={element.description ? element.description.slice(0, 85) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
                         </div>
                     })}
                 </div>
                 <div className="container d-flex justify-content-between">
                     <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Previous</button>
                     <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 5)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-                    {console.log(Math.ceil(this.state.totalResults / this.props.pageSize))}
                 </div>
             </div>
         )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,14 +4,16 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
     render() {
-        let { title, description, imageUrl, newsUrl } = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className='my-3'>
                 <div className="card">
-                    <img src={imageUrl} className="card-img-top" alt="newsImage" />
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}>{source}</span>
+                    <img src={imageUrl ? imageUrl : "https://images.unsplash.com/photo-1614028674490-d12fd4eaa168?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&height=600&q=80"} className="card-img-top" alt="newsImage" />
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
+                        <h5 className="card-title">{title} </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
